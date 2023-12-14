c5a0f729c369977908a204d24b4332ede2f9c2fd
Jiri Vlasak <jirivlasak9@gmail.com>
2023-12-14 14:57:30 +0100

    Removed unused files

--- meta/process_parliament_hearings.sh
+++
@@ -1,8 +0,0 @@
-TOPDIR=/storage/brno12-cerit/home/xkotou06/
-
-
-cd $TOPDIR/knn-whisper
-pip install -r requirements.txt
-
-# -- do your amazing stuf here
-python ./src/process_parliament_dataset.py
